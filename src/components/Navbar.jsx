f0a762777a9cf0076dd58933e11a41ea9d0a1b8e
rishab <kumarrishab451@gmail.com>
2024-06-10 17:45:58 +0530

    Intial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React,{useCallback,useState} from 'react'
+import {Link} from 'react-router-dom'
+
+import {styles} from '../style'
+import { navLinks } from '../constants'
+import { logo,menu,close } from '../assets'
+
+const Navbar = () => {
+  return (
+    <nav className={`${styles.paddingX}
+    `}
+    >
+
+    </nav>
+  )
+}
+
+export default Navbar
